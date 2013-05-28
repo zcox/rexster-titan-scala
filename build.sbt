b8f64474b7b86f13982f7c298d3688f6d91aca8d
Zach Cox <zcox522@gmail.com>
2013-05-28 14:46:21 -0500

    RexsterGraph example

--- build.sbt
+++ build.sbt
@@ -7,9 +7,14 @@ version := "0.1-SNAPSHOT"
 scalaVersion := "2.9.1"
 
 libraryDependencies ++= {
+  val rexster = "2.3.0"
+  val titan = "0.3.1"
   val slf4j = "1.7.5"
   Seq(
-    "com.tinkerpop.rexster" % "rexster-protocol" % "2.3.0",
+    "com.tinkerpop.rexster" % "rexster-protocol" % rexster,
+    //"com.tinkerpop.rexster" % "rexster-server" % rexster,
+    //"com.thinkaurelius.titan" % "titan-core" % titan,
+    "com.tinkerpop.blueprints" % "blueprints-rexster-graph" % rexster,
     "org.slf4j" % "slf4j-api" % slf4j,
     "org.slf4j" % "jcl-over-slf4j" % slf4j,
     "org.slf4j" % "log4j-over-slf4j" % slf4j,
@@ -22,4 +27,5 @@ ivyXML :=
   <dependencies>
     <exclude org="log4j" name="log4j" />
     <exclude org="commons-logging" name="commons-logging" />
+    <exclude org="org.slf4j" name="slf4j-log4j12" />
   </dependencies>
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,11 @@
 package com.pongr
 
 import com.tinkerpop.rexster.client.RexsterClientFactory
-import grizzled.slf4j.Logging
 import scala.collection.JavaConversions._
+import grizzled.slf4j.Logging
 
-object Main extends App with Logging {
+//https://github.com/tinkerpop/rexster/wiki/RexPro-Java
+object RexsterClientMain extends App with Logging {
   val client = RexsterClientFactory.open("localhost", "graph")
 
   val names: Seq[String] = client.execute("g.V.name").toSeq
@@ -15,3 +16,32 @@ object Main extends App with Logging {
 
   client.close()
 }
+
+//https://github.com/tinkerpop/blueprints/wiki/Rexster-Implementation
+import com.tinkerpop.blueprints.impls.rexster.RexsterGraph
+import com.tinkerpop.blueprints.Direction._
+object RexterGraphMain extends App with Logging {
+  val graph = new RexsterGraph("http://localhost:8182/graphs/graph")
+
+  val names = graph.getVertices.map(v => Option(v.getProperty("name"))).flatten
+  debug("%d names: %s" format (names.size, names.mkString("[", ",", "]")))
+
+  val zachLikes = graph.getVertices("name", "Zach").flatMap(_.getVertices(OUT, "likes")).map(v => Option(v.getProperty("name"))).flatten
+  debug("Zach likes %d things: %s" format (zachLikes.size, zachLikes.mkString("[", ",", "]")))
+}
+
+//An attempt to expose in-memory Titan via RexPro...
+/*import com.thinkaurelius.titan.core.TitanFactory
+import com.tinkerpop.rexster.server.{DefaultRexsterApplication, RexProRexsterServer}
+import org.apache.commons.configuration.{BaseConfiguration, XMLConfiguration}
+
+object RexProServerWithInMemoryTitanGraph extends App {
+  val conf = new BaseConfiguration()
+  conf.setProperty("storage.backend","inmemory")
+  val graph = TitanFactory.open(conf) //this throws an NPE...
+
+  val app = new DefaultRexsterApplication("graph", graph)
+  val props = new XMLConfiguration()
+  val server = new RexProRexsterServer(props)
+  server.start(app)
+}*/
